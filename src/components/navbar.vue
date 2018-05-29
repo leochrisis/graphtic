4f7d1a92ff6896afd8ab2f24bd8513795e15250f
Leochrisis <leonardo.chfc@gmail.com>
2018-05-28 21:41:00 -0300

    Create navbar component

---
+++ src/components/navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="navbar">
+    <div class="container">
+      <div class="navbar-brand">
+        <a class="navbar-item">
+          GRAPHTIC
+        </a>
+      </div>
+      <div id="navbarMenuHeroA" class="navbar-menu">
+        <div class="navbar-end">
+          <router-link to="/" class="navbar-item">
+            Home
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: 'Navbar'
+}
+</script>
