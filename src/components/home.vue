43e7b4360daa67d52c11265f9ba46af8c219c750
Leochrisis <leonardo.chfc@gmail.com>
2018-05-28 21:43:24 -0300

    Setup home component

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,11 +1,28 @@
 <template>
   <div>
-    Hello
+    <section class="hero is-primary is-fullheight">
+      <div class="hero-head">
+        <navbar></navbar>
+      </div>
+      <div class="hero-body">
+        <div class="container has-text-centered">
+          <p class="subtitle has-text-grey">Algoritmos implementados</p>
+          <div class="column is-4 is-offset-4">
+            <div class="box"> 
+              <router-link to="/bresenham" class="button is-link is-rounded">Bresenham</router-link>          
+            </div>      
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script type="text/javascript">
-  export default {
-    name: 'Home'
-  }
+import Navbar from './navbar.vue'
+export default {
+  name: 'Home',
+
+  components: {Navbar}
+}
 </script>
