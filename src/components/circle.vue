6fa27ed4ff4a054be2cede075626aecba9c9dbeb
Leochrisis <leonardo.chfc@gmail.com>
2018-05-28 23:02:00 -0300

    Add draw grid method

--- src/components/circle.vue
+++ src/components/circle.vue
@@ -20,6 +20,23 @@ export default {
 
   methods: {
     drawGrid () {
+      this.canva = this.$refs.el
+      this.context = this.canva.getContext('2d')
+
+      this.context.beginPath();
+      this.context.fillStyle = 'black';
+      this.context.lineWidth = 3;
+      this.context.strokeStyle = 'white';
+
+      for (var i = 0; i < 15; i++) {
+        for (var j = 0; j < 15; j++) {
+          var x = j * 20;
+          var y = i * 20;
+          this.context.rect(x, y, 20, 20);
+          this.context.fill();
+          this.context.stroke();
+        }
+      }
     },
 
     drawCircle () {
